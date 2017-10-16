6a00b94718926975b2f3776b31a5e41c5ef2d501
jmarinb <jcmcesar45@hotmail.com>
2017-10-16 15:54:03 -0500

    nueva modificacion

--- test/src/test/Main.java
+++ test/src/test/Main.java
@@ -4,6 +4,36 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Hello, World"); 
+		int a= 15;
+		int b= 3;
+		int resultado = 0;
+		
+		resultado = a+b;
+		System.out.println("Suma:"+resultado); 
+		float d = 3.1415456f;
+		float c = 10.34434f;
+		float resf = 0.0f;
+		
+		resf =  d+a;
+		
+		System.out.println("Suma flotante:"+resf); 
+		
+		String cadena = "Parangaricutirimicuaro";
+		System.out.println("//////////////////////////////////////cadena///////////////");
+		String cadenaCesar="ABCDEFGHIJKLMNÑOPQRSTUVWXYZ";
+		int espacio =3;
+		
+		System.out.println(cadena);
+		for(int x= 0; x<cadena.length(); x++)
+		{
+			System.out.println(cadena.charAt(x));
+			cadena = cadena.replace(cadena.charAt(x), cadenaCesar.charAt(x+espacio));
+		}
+		
+		
+		
+		
+		
 
 	}
 
